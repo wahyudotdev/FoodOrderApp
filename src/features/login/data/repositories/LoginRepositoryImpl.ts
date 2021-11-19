1c4ecfb56357c195a78b06c0528d6cc538a400b9
wahyudotdev <wahyu.smallker@gmail.com>
2021-11-19 14:43:14 +0700

    fix: fix login repository impl

--- src/features/login/data/repositories/LoginRepositoryImpl.ts
+++ src/features/login/data/repositories/LoginRepositoryImpl.ts
@@ -1,28 +1,34 @@
-import {Failure} from '../../../../core/error/failure';
-import {Either} from '../../../../core/usecases/Either';
+import {baseUrl} from '../../../../../src/config/ApiUrl';
+import {Failure, ServerFailure} from '../../../../core/error/failure';
+import {Either, left, right} from '../../../../core/usecases/Either';
 import {LoginRepository} from '../../domain/repositories/LoginRepository';
-import {User} from '../entities/LoginResponse';
+import {Convert, User} from '../entities/LoginResponse';
 
 export class LoginRepositoryImpl implements LoginRepository {
   async login(
     _username: string,
     _password: string,
   ): Promise<Either<Failure, User>> {
-    const formData = new FormData();
-    formData.append('username', _username);
-    formData.append('password', _password);
-    // let response = await fetch(baseUrl + '/login', {
-    //   method: 'POST',
-    //   headers: {
-    //     'content-type': 'multipart/form-data',
-    //   },
-    // });
-    throw new Error();
-    // if (response.status === 200) {
-    //   let result = Convert.toLoginResponse();
-    //   return right(result.data);
-    // } else {
-    //   return left(new ServerError('Server error', 'Error response'));
-    // }
+    try {
+      const formData = new FormData();
+      formData.append('username', _username);
+      formData.append('password', _password);
+      let response = await fetch(baseUrl + '/login', {
+        method: 'POST',
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+        body: formData,
+      });
+      let jsonString = await response.text();
+      let result = Convert.toLoginResponse(jsonString);
+      if (result.status === 200) {
+        return right(result.data);
+      } else {
+        return left(new ServerFailure(result.message));
+      }
+    } catch (error) {
+      return left(new ServerFailure(String(error)));
+    }
   }
 }
